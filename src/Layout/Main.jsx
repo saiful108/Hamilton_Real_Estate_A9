15a9c083c5bf773825dd90f25270cc313b35a198
saiful108 <sahabah108@gmail.com>
2024-06-10 14:10:37 +0600

    add navbar some changes

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../Componets/Navbar";
 
 const Main = () => {
     return (
-        <div>
+        <div className="max-w-6xl mx-auto bg-fuchsia-500">
+            <Navbar></Navbar>
            <Outlet></Outlet> 
         </div>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { HelmetProvider } from 'react-helmet-async'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
      <HelmetProvider>
-     <RouterProvider router={router} />
+     <RouterProvider  router={router} />
      </HelmetProvider>
       </React.StrictMode>,
 )
